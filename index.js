04f58c74821739e66cbe3c6a27493adff7072b57
Jigar Polra <jigarpolra@live.co.uk>
2018-07-15 16:43:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+let currentUser = "Jigar"
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
+let excitedWelcomeMessage = welcomeMessage.toUpperCase()
+let shortGreeting = "Welcome, " + currentUser.charAt(0).toUpperCase() + "!"
+
